46df5db9b446b789a7a266b94016f92d87c847db
cfticmongo <cfticmongo@gmail.com>
2021-09-29 19:18:50 +0200

    arit

---
+++ aritmeticos.js
@@ -0,0 +1,57 @@
+// Operadores aritméticos
+
+// + y -
+
+let result = 6;
+let factor = 2;
+
+let points = result - factor;
+console.log(points);
+
+// El operador suma si los tipos son string concatena
+
+let name = 'Laura';
+let message = 'Hola';
+
+let finalMessage =  message + ' ' + name;
+console.log(finalMessage);
+
+result = 10;
+factor = '3';
+
+points = result + factor;
+console.log(points);
+
+points = result - factor;
+console.log(points);
+
+// * y /
+
+let a = 10;
+let b = 2;
+
+result = a / b;
+console.log(result);
+
+a = 'Hello';
+result = a * b;  // asignar un valor especial NaN
+console.log(result);
+
+// Resto %
+
+let c = 13 % 2;
+console.log(c);
+
+// Incremento ++ (sumar una unidad al valor)
+
+c = 1;
+console.log(c++) // Postincremento
+console.log(c);
+
+let d = 10;
+console.log(++d); // Preincremento
+
+// Decremento -- (resta una unidad)
+
+// Idem al anterior
+
